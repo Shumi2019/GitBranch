0fc56b834942a64795bc61b1cdaebc95bdab7707
shumi34 <shumi.A345@gmail.com>
2025-12-09 14:00:05 -0500

    this is my first commit

--- src/test/java/shumi1.java
+++ src/test/java/shumi1.java
@@ -1,6 +1,7 @@
-public class class1 {
+public class shumi1 {
 
     public static void main(String[] args) {
         System.out.println("This is my test1 for shumi");
+        System.out.println("This is my test2 for shumi");
     }
 }
